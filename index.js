ec3686d80a1ca19c124cf63b4fe5c02dbc1af950
ichala <alijandhd@gmail.com>
2022-04-04 11:28:11 +0100

    Remove Console Log

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 document.querySelector('.tool-button').addEventListener('click', () => {
   const menu = document.getElementById('mobile-menu_container');
-  console.log(menu);
   menu.classList.toggle('hide');
 });
 document.querySelector('.close_menu_icon').addEventListener('click', () => {
