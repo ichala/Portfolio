931ea166fc8c4f1320e8f702d13349cf3527b083
ichala <alijandhd@gmail.com>
2022-04-04 11:26:01 +0100

    Fix Linters Errors

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-document.querySelector('.tool-button').addEventListener('click',()=>{
-    const menu = document.getElementById('mobile-menu_container');
-    console.log(menu);
-    menu.classList.toggle("hide");
-})
-document.querySelector('.close_menu_icon').addEventListener('click',()=>{
-    document.getElementById('mobile-menu_container').classList.toggle("hide")
-})
-document.querySelectorAll('.nav-link-mobile').forEach(
-  (link)=>{link.addEventListener('click',()=>{
-    document.getElementById('mobile-menu_container').classList.toggle("hide")}
-  )})
+document.querySelector(".tool-button").addEventListener("click", () => {
+  const menu = document.getElementById("mobile-menu_container");
+  console.log(menu);
+  menu.classList.toggle("hide");
+});
+document.querySelector(".close_menu_icon").addEventListener("click", () => {
+  document.getElementById("mobile-menu_container").classList.toggle("hide");
+});
+document.querySelectorAll(".nav-link-mobile").forEach((link) => {
+  link.addEventListener("click", () => {
+    document.getElementById("mobile-menu_container").classList.toggle("hide");
+  });
+});
